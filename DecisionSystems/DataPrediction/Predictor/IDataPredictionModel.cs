957a3937fc4852982a7bc42931268da91763505c
johannesegger <j.egger@posteo.at>
2020-09-19 20:37:07 +0200

    Add data prediction page

---
+++ DecisionSystems/DataPrediction/Predictor/IDataPredictionModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DecisionSystems.DataPrediction.Predictor
+{
+    public interface IDataPredictionModel
+    {
+        double Test(double independentValue);
+
+        List<DataPoint> Test(IEnumerable<double> independentValues)
+        {
+            return independentValues
+                .Select(v => new DataPoint(v, Test(v)))
+                .ToList();
+        }
+    }
+}
