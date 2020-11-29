fbbe839e17a135f58f579f0bb2711888b1991f1d
johannesegger <j.egger@posteo.at>
2020-11-29 21:49:05 +0100

    Extract method to swap to array elements

---
+++ DecisionSystems/ArrayExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DecisionSystems
+{
+    public static class ArrayExtensions
+    {
+        public static void Swap<T>(this IList<T> list, int idx1, int idx2)
+        {
+            (list[idx1], list[idx2]) = (list[idx2], list[idx1]);
+        }
+    }
+}
